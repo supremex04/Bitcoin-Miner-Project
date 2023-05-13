0bf33dfbc0aaab917cc6ed16448ff83ab8f6e046
supremex04 <bhandarisupreme7@gmail.com>
2023-05-13 18:44:54 +0545

    Update

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from hashlib import sha256
 import time
 NONCE_LIMIT =100000000
+block_height =0
 difficulty = 3
 tx = sha256(b"transactions_are_here").hexdigest()
 last_block = sha256(b"Last_block").hexdigest()
-print(tx)
-def mine(block_height, last_block, tx, nonce, difficulty):
+def mine(block_height, last_block, tx,difficulty):
     for nonce in range(NONCE_LIMIT):
-        base = str(block_height) + last_hash+ tx + str(nonce)
+        base = str(block_height) + last_block+ tx + str(nonce)
         calc_digest = sha256(base.encode()).hexdigest()
+        if calc_digest.startswith("0"*difficulty):
+            print(f"Block mined: {calc_digest} \nNonce is: {nonce}")
+            return nonce
         
-
+mine(block_height, last_block, tx, difficulty)
 
 
 
